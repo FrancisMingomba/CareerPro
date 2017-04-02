6208eda50c15de41ca52cca0d288ee3b9cdd6f9d
francis mingomba <francis mingomba>
2017-04-02 18:28:00 -0500

    Initial Commit

---
+++ CareerPro.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CareerPro.Web.MvcApplication" Language="C#" %>
---
+++ CareerPro.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CareerPro.Web.Startup))]
+namespace CareerPro.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
