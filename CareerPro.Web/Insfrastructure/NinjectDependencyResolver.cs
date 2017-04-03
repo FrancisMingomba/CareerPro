322a5784b8b4f86dd7262073819bc054bc8f6c16
FrancisMingomba <mingomba@gmail.com>
2017-04-02 19:13:13 -0500

    Installation of Ninject, Moq and reference dependencies

---
+++ CareerPro.Web/Insfrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,36 @@
+﻿using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CareerPro.Web.Insfrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+
+        private void AddBindings()
+        {
+            //bindings go here
+        }
+    }
+}
